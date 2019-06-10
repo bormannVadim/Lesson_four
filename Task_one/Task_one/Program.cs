0eb849d224b4041411256efed983d4d5b043491c
bormannVadim <freedom-mv47@rambler.ru>
2019-06-10 17:10:21 +0300

    добавил создание массива

---
+++ Task_one/Task_one/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Task_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            FillArray(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.WriteLine((i + 1) + ": " + arr[i]);
+
+        }
+
+        static void FillArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Random r = new Random();
+                arr[i] = r.Next(100);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
