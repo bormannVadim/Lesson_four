effd97b638f40693a7fb2a467c123b4f7965d4ef
bormannVadim <freedom-mv47@rambler.ru>
2019-06-10 17:23:11 +0300

    сделал 1 задание деление одного из чисел пары на 3

--- Task_one/Task_one/Program.cs
+++ Task_one/Task_one/Program.cs
@@ -7,12 +7,14 @@ namespace Task_one
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[5];
+            // Савенко В
+            int[] arr = new int[20];
             FillArray(arr);
 
             for (int i = 0; i < arr.Length; i++)
                 Console.WriteLine((i + 1) + ": " + arr[i]);
 
+            Console.WriteLine("В массиве "+DivideBy3(arr)+" пар, где только одно из двух чисел делится на 3...");
         }
 
         static void FillArray(int[] arr)
@@ -20,9 +22,22 @@ namespace Task_one
             for (int i = 0; i < arr.Length; i++)
             {
                 Random r = new Random();
-                arr[i] = r.Next(100);
-                Thread.Sleep(10);
+                arr[i] = r.Next(-10000,10000);
+                Thread.Sleep(100);
             }
+        }
+
+        static int DivideBy3(int[] arr)
+        {
+            int counter = 0;
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                if (arr[i] % 3 == 0 && arr[i + 1] % 3 != 0)
+                    counter++;
+                else if (arr[i] % 3 != 0 && arr[i + 1] % 3 == 0)
+                    counter++;
+            }
+            return counter;
         }
     }
 }
